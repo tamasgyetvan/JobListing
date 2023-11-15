d449ce6d579a464c14c0615cdf2f56ecd1561ebe
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-11-15 20:39:08 +0100

    Amend JobCard styling

--- JobListing/src/App.tsx
+++ JobListing/src/App.tsx
@@ -2,28 +2,16 @@ import { Header } from "./components/Header";
 import { JobCard } from "./components/JobCard";
 import "./scss/base/_base.scss";
 import "./scss/layout/_main.scss";
+import jobData from "../data.json";
 
 function App() {
-  const obj = {
-    id: 1,
-    company: "Photosnap",
-    logo: "./images/photosnap.svg",
-    new: true,
-    featured: true,
-    position: "Senior Frontend Developer",
-    role: "Frontend",
-    level: "Senior",
-    postedAt: "1d ago",
-    contract: "Full Time",
-    location: "USA Only",
-    languages: ["HTML", "CSS", "JavaScript"],
-    tools: [],
-  };
   return (
     <>
       <Header></Header>
       <main>
-        <JobCard job={obj}></JobCard>
+        {jobData.map((job) => {
+          return <JobCard key={job.id} job={job}></JobCard>;
+        })}
       </main>
     </>
   );
--- JobListing/src/components/JobCard.tsx
+++ JobListing/src/components/JobCard.tsx
@@ -7,7 +7,7 @@ type JobCardProps = {
 export const JobCard: React.FC<JobCardProps> = (props) => {
   const { job } = props;
   return (
-    <div key={job.id} className="jobCard">
+    <div className={job.featured ? "jobCard featured" : "jobCard"} key={job.id}>
       <div className="jobInfo">
         <img className="jobLogo" src={job.logo} alt="Job logo" />
         <div className="jobDetails">
@@ -17,15 +17,21 @@ export const JobCard: React.FC<JobCardProps> = (props) => {
           <h1 className="jobPosition">{job.position}</h1>
           <div>
             <h2>{job.postedAt}</h2>
+            <p>&#8226;</p>
             <h2>{job.contract}</h2>
+            <p>&#8226;</p>
             <h2>{job.location}</h2>
           </div>
         </div>
       </div>
       <div className="jobTags">
         <h2>{job.role}</h2>
+        <h2>{job.level}</h2>
         {job.languages.map((lang) => {
           return <h2>{lang}</h2>;
+        })}
+        {job.tools.map((tool) => {
+          return <h2>{tool}</h2>;
         })}
       </div>
     </div>
