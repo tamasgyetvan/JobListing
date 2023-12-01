21ddf29c6fa25fe8765b491628ffc742db4234d5
tamasgyetvan <117265815+tamasgyetvan@users.noreply.github.com>
2023-12-01 13:09:01 +0100

    Add levelFilter to Filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -8,31 +8,35 @@ type FilterProps = {
 
 export const Filter: React.FC<FilterProps> = ({ applyRoleFilter }) => {
   let roles: Array<string> = [];
+  let levels: Array<string> = [];
 
-  const handleCheckboxInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleRoleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!roles?.includes(e.target.value)) {
       roles = [...roles, e.target.value];
     } else {
       roles = roles.filter((role) => role !== e.target.value);
     }
-    console.log(roles);
+  };
+
+  const handleLevelInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!levels.includes(e.target.value)) {
+      levels = [...levels, e.target.value];
+    } else {
+      levels.filter((level) => level !== e.target.value);
+    }
   };
   return (
     <form className="filter">
       <h2>Role</h2>
       <div className="roleFilter">
-        <Checkbox
-          value={"Frontend"}
-          handleInput={handleCheckboxInput}
-        ></Checkbox>
-        <Checkbox
-          value={"Backend"}
-          handleInput={handleCheckboxInput}
-        ></Checkbox>
-        <Checkbox
-          value={"Fullstack"}
-          handleInput={handleCheckboxInput}
-        ></Checkbox>
+        <Checkbox value={"Frontend"} handleInput={handleRoleInput}></Checkbox>
+        <Checkbox value={"Backend"} handleInput={handleRoleInput}></Checkbox>
+        <Checkbox value={"Fullstack"} handleInput={handleRoleInput}></Checkbox>
+      </div>
+      <div className="levelFilter">
+        <Checkbox value={"Junior"} handleInput={handleLevelInput}></Checkbox>
+        <Checkbox value={"Midweight"} handleInput={handleLevelInput}></Checkbox>
+        <Checkbox value={"Senior"} handleInput={handleLevelInput}></Checkbox>
       </div>
       <button
         onClick={(e) => {
