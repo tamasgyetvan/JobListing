dd216a722b3ea6680a53e8dc1fdd2a17808302e9
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-12-08 10:03:11 +0100

    Add contractTypeFilter to Filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -12,6 +12,7 @@ type FilterProps = {
 export const Filter: React.FC<FilterProps> = ({ applyFilters }) => {
   let roles: Array<string> = [];
   let levels: Array<string> = [];
+  let contractTypes: Array<string> = [];
 
   const handleRoleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!roles?.includes(e.target.value)) {
@@ -28,6 +29,15 @@ export const Filter: React.FC<FilterProps> = ({ applyFilters }) => {
       levels = levels.filter((level) => level !== e.target.value);
     }
   };
+  const handleContractTypeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!contractTypes.includes(e.target.value)) {
+      contractTypes = [...contractTypes, e.target.value];
+    } else {
+      contractTypes = contractTypes.filter(
+        (contractType) => contractType !== e.target.value
+      );
+    }
+  };
   return (
     <form className="filter">
       <h2>Role</h2>
@@ -40,6 +50,20 @@ export const Filter: React.FC<FilterProps> = ({ applyFilters }) => {
         <Checkbox value={"Junior"} handleInput={handleLevelInput}></Checkbox>
         <Checkbox value={"Midweight"} handleInput={handleLevelInput}></Checkbox>
         <Checkbox value={"Senior"} handleInput={handleLevelInput}></Checkbox>
+      </div>
+      <div className="contractTypeFilter">
+        <Checkbox
+          value={"Full Time"}
+          handleInput={handleContractTypeInput}
+        ></Checkbox>
+        <Checkbox
+          value={"Contract"}
+          handleInput={handleContractTypeInput}
+        ></Checkbox>
+        <Checkbox
+          value={"Part Time"}
+          handleInput={handleContractTypeInput}
+        ></Checkbox>
       </div>
       <button
         onClick={(e) => {
